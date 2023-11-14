2925619c44ddcc190a2be3dfb2c5a695f3200cfe
“anishkumar127” <anishbishnoi127@gmail.com>
2023-11-14 16:33:09 +0530

    fixed schema based state save

--- src/controllers/unmatched-controllers/unmatchedDetectController.ts
+++ src/controllers/unmatched-controllers/unmatchedDetectController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { CompanyOpen } from "../../models/company.model";
 import { VendorOpen } from "../../models/vendor.model";
+import { Soa } from "../../models/soa.model";
+import { CaseP } from "../../models/cases/case_p.model";
 
 export const unmatchedDetectController = async (
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -8,50 +10,90 @@ export const unmatchedDetectController = async (
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   res: Response
 ) => {
-    const masterFileCompanyOpen = await CompanyOpen.find();
-    // console.log(masterFileCompanyOpen);
+  const fullDetailsFile = await Soa?.find();
+  const masterFileCompanyOpen = await CompanyOpen?.find();
+  const vendorOpenData = await VendorOpen?.find();
 
-    const extractedMasterData = masterFileCompanyOpen.map((item) => ({
-      vendorName: item?.data?.vendor_name,
-      documentNo: item?.data?.document_no,
-      invoiceNumber: item?.data?.invoice_number,
-      closing_balance: item?.data?.closing_balance,
-      completeInfo:item
-    }));
+  const extractedMasterData = masterFileCompanyOpen?.map((item) => ({
+    vendorName: item?.data?.vendor_name,
+    documentNo: item?.data?.document_no,
+    invoiceNumber: item?.data?.invoice_number,
+    closing_balance: item?.data?.closing_balance,
+    completeInfo: item,
+    ID :item?.user
+  }));
 
-    const vendorOpenData = await VendorOpen?.find();
-    const extractedVendorData = vendorOpenData?.map((item:any) => ({
-      vendorName: item?.data?.vendor_name,
-      documentNo: item?.data?.document_no,
-      invoiceNumber: item?.data?.invoice_number,
-      closing_balance: item?.data?.closing_balance,
-      completeInfo:item
-    }));
+  const extractedVendorData = vendorOpenData?.map((item: any) => ({
+    vendorName: item?.data?.business_partner_name,
+    documentNo: item?.data?.document_no,
+    invoiceNumber: item?.data?.invoice_number,
+    closing_balance: item?.data?.closing_balance,
+    completeInfo: item,
+  }));
 
-    // const matchedInvoiceNumber = extractedMasterData?.filter((item) =>
-    //   extractedVendorData
-    //     ?.map((x) => x?.completeInfo?.mixed_data?.invoice_number)
-    //     ?.includes(item?.invoiceNumber)
-    // );
-
-    const matchedInvoiceNumberWithDifference = extractedMasterData?.filter((masterItem) => {
+  const matchedInvoiceNumberWithDifference = extractedMasterData
+    ?.map((masterItem) => {
       const vendorItem = extractedVendorData?.find(
-        (vendor) => vendor?.completeInfo?.mixed_data?.invoice_number === masterItem?.invoiceNumber
+        (vendor) =>
+          vendor?.completeInfo?.mixed_data?.invoice_number ===
+          masterItem?.invoiceNumber
       );
-    
+
       if (vendorItem && masterItem?.invoiceNumber === "anish") {
-        const masterAmount = parseFloat(masterItem?.closing_balance || '0'); 
-        const vendorAmount = parseFloat(vendorItem?.completeInfo?.mixed_data?.closing_balance || '0'); 
-    
+        const masterAmount = parseFloat(masterItem?.closing_balance || "0");
+        const vendorAmount = parseFloat(
+          vendorItem?.completeInfo?.mixed_data?.closing_balance || "0"
+        );
+
         const amountDifference = masterAmount - vendorAmount;
-        console.log("m",masterAmount,"v",vendorAmount);
-        return { ...masterItem, amountDifference };
+
+        return { masterItem, vendorItem, amountDifference };
       }
-      return false;
-    }).filter(Boolean)
-    
+      return null;
+    })
+    .filter(Boolean);
 
-   return res.json({matchedInvoiceNumberWithDifference})
-    // return res.json({ extractedMasterData,extractedVendorData });
+  // -3066.67
+  if (matchedInvoiceNumberWithDifference[0]?.amountDifference !== 0) {
+    const fullDetailsFileData = fullDetailsFile?.map((item: any) => ({
+      documentNo: item?.data?.document_no,
+      invoiceNumber: item?.data?.invoice_number,
+      completeInfo: item,
+    }));
+    console.log(fullDetailsFileData);
+    const matchedFullDetails = fullDetailsFileData
+      ?.map((item) => {
+        const masterItem = extractedMasterData?.find((master) => {
+          return (
+            master?.completeInfo?.mixed_data?.invoice_number ===
+            item?.invoiceNumber
+          );
+        });
+
+        if (masterItem && item?.invoiceNumber === "anish") {
+          return { ...item };
+        }
+        return null;
+      })
+      .filter(Boolean);
+
+      try {
+        console.log(extractedMasterData)
+        await CaseP.create({
+          user: extractedMasterData[0]?.ID,
+          // filename: originalname,
+          // data: excelData[i],
+          mixed_data:matchedFullDetails[0]?.completeInfo.mixed_data
+        } as any);
+      } catch (error) {
+        console.log("error case p",error);
+      }
+    return res.json({
+      matchedInvoiceNumberWithDifference,
+      matchedFullDetails,
+    });
+  }
+
+  return res.json({ matchedInvoiceNumberWithDifference });
+  // return res.json({ extractedMasterData,extractedVendorData });
 };
-
--- src/routes/unmatched/unmatchedDetectRoutes.ts
+++ src/routes/unmatched/unmatchedDetectRoutes.ts
@@ -1,8 +1,11 @@
 import express from "express";
 const router = express.Router();
 import { unmatchedDetectController } from "../../controllers/unmatched-controllers/unmatchedDetectController";
+import { generateCasePController } from "../../controllers/unmatched-controllers/generateCasePController";
 
 
 router.get("/unmatched",unmatchedDetectController);
 
+// GET P Case.
+router.get('/case-p',generateCasePController)
 export default router;
