dc7f6a75d0cecf1aae21ad805f186902677def8c
“anishkumar127” <anishbishnoi127@gmail.com>
2023-11-21 23:37:19 +0530

    cookie domain specific

--- src/controllers/user-controllers/userControllers.ts
+++ src/controllers/user-controllers/userControllers.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import { User } from "../../models/user.model";
-import { v4 as uuidv4 } from "uuid";
 import { setUser } from "../../services/auth";
 
 export const userSignUpController = async (req: Request, res: Response) => {
