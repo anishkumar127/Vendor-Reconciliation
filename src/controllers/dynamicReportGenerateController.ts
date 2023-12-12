c92c784974f123274c0d05c00d26caf0d625842f
Anish Kumar <anishbishnoi127@gmail.com>
2023-12-12 23:16:16 +0530

    p & k case development phase

--- src/controllers/dynamicReportGenerateController.ts
+++ src/controllers/dynamicReportGenerateController.ts
@@ -3,6 +3,7 @@ import { RecentIds } from "../models/mixed/RecentIds.model";
 import { RequestHandler } from "express";
 import { getUser } from "../services/auth";
 import PCase from "../models/cases/PCase.model";
+import KCase from "../models/cases/KCase.model";
 
 // IN AMOUNT NUMBER HAS THE COMMA (,) REMOVING.
 function removeCommas(value: any) {
@@ -104,11 +105,17 @@ export const dynamicReportGenerateController: RequestHandler = async (
     if (!data) return res.status(500).json({ error: "server query error!" });
 
     const matchedData: any = [];
+
+    const matchPCaseData: any = [];
+
+    const matchKCaseData: any = [];
+    let isPCaseTrue: boolean = false;
+    let isKCaseTrue: boolean = false;
+
     for (let i = 0; i < data?.length; i++) {
       const first = removeCommas(data[i]?.data["Closing Balance"]);
       const second = removeCommas(data[i]?.result?.data["Closing Balance"]);
       const diff = +second - +first;
-      // console.log({ diff });
       if (diff) {
         const lastCollection = await getModelByString(`${email}@complete`);
         if (lastCollection) {
@@ -117,8 +124,28 @@ export const dynamicReportGenerateController: RequestHandler = async (
             "data.Invoice Number": invoiceNumber,
             uniqueId: recentIds?.detailsId,
           });
-          if (lastData) {
-            matchedData.push(lastData.map((item: any) => item.data));
+
+          for (let i = 0; i < lastData?.length; i++) {
+            //  MATCHING THE DOCUMENT TYPE.
+            const documentNumber = lastData[i]?.data["Document Number"];
+            if (
+              documentNumber &&
+              (documentNumber.startsWith("PID") ||
+                documentNumber.endsWith("PID") ||
+                documentNumber.includes("PID"))
+            ) {
+              isPCaseTrue = true;
+              matchPCaseData.push(lastData[i]);
+            } else if (
+              documentNumber &&
+              (documentNumber.startsWith("TDS") ||
+                documentNumber.endsWith("TDS") ||
+                documentNumber.includes("TDS"))
+            ) {
+              isKCaseTrue = true;
+              console.log("K", lastData[i]);
+              matchKCaseData.push(lastData[i]);
+            }
           }
         }
       }
@@ -127,28 +154,58 @@ export const dynamicReportGenerateController: RequestHandler = async (
     //  GENERATE P CASE.
     const flattenedData = await matchedData.flat();
 
-    let idx: number = 1;
-    for (const item of flattenedData) {
-      const pCaseInstance = await new PCase({
-        user: new mongoose.Types.ObjectId(_id),
-        uniqueId: recentIds?.masterId,
-        SNO: idx++,
-        "Company Code": item["Company Code"],
-        "Document Number": item["Document Number"],
-        "Document Date": item["Document Date"],
-        "Invoice Number": item["Invoice Number"],
-        "Grn Number": item["Grn Number"],
-        "Debit Amount(INR)": item["Debit Amount(INR)"],
-      });
+    // TEST P CASE
+    const pCaseDataFlat = await matchPCaseData.flat();
 
-      try {
-        await pCaseInstance.save();
-        // const savedInstance = await pCaseInstance.save();
-        // console.log(`Data saved: ${savedInstance}`);
-      } catch (error) {
-        console.error(`Error saving data: ${error}`);
+    // TEST K CASE
+    const kCaseDataFlat = await matchKCaseData.flat();
+
+    if (isPCaseTrue) {
+      let idx: number = 1;
+      for (const item of pCaseDataFlat) {
+        const pCaseInstance = await new PCase({
+          user: new mongoose.Types.ObjectId(_id),
+          uniqueId: recentIds?.masterId,
+          SNO: idx++,
+          "Company Code": item.data["Company Code"],
+          "Document Number": item.data["Document Number"],
+          "Document Date": item.data["Document Date"],
+          "Invoice Number": item.data["Invoice Number"],
+          "Grn Number": item.data["Grn Number"],
+          "Debit Amount(INR)": item.data["Debit Amount(INR)"],
+        });
+
+        try {
+          await pCaseInstance.save();
+        } catch (error) {
+          console.error(`Error saving data: ${error}`);
+        }
       }
     }
+
+    if (isKCaseTrue) {
+      let idx: number = 1;
+      for (const item of kCaseDataFlat) {
+        const kCaseInstance = await new KCase({
+          user: new mongoose.Types.ObjectId(_id),
+          uniqueId: recentIds?.masterId,
+          SNO: idx++,
+          "Company Code": item.data["Company Code"],
+          "Document Number": item.data["Document Number"],
+          "Document Date": item.data["Document Date"],
+          "Invoice Number": item.data["Invoice Number"],
+          "Grn Number": item.data["Grn Number"],
+          "Debit Amount(INR)": item.data["Debit Amount(INR)"],
+        });
+
+        try {
+          await kCaseInstance.save();
+        } catch (error) {
+          console.error(`Error saving data: ${error}`);
+        }
+      }
+    }
+
     return res.status(200).json({
       message: "ok",
       data: flattenedData,
