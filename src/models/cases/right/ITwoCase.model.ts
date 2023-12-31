fe86cbedb6982a0b0d8733f7f79ef8e157f4b369
Anish Kumar <anishbishnoi127@gmail.com>
2023-12-31 22:01:30 +0530

    Right - P2,K2,G2,I2,L3,M4

---
+++ src/models/cases/right/ITwoCase.model.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const yourSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    uniqueId: { type: String, required: true },
+    SNO: { type: Number },
+    "Company Code": { type: String, required: false },
+    "Vendor Code": { type: String, required: false },
+    Year: { type: String, required: false },
+    "Document Number": { type: String, required: false },
+    "Document Date": { type: String, required: false },
+    "Due Date": { type: String, required: false },
+    "Invoice Amount": { type: String, required: false },
+    Amount: { type: String },
+  },
+  { timestamps: true }
+);
+
+const ITwoCase = mongoose.model("ITwoCase", yourSchema);
+
+export default ITwoCase;
